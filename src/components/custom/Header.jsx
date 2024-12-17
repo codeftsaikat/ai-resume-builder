0308aa16bf74dcadb742835cd958c9aa4abfe5a2
codeftsaikat <soikatdey18@gmail.com>
2024-12-17 11:56:35 -0800

    Working on the header section

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
+import { UserButton, useUser } from "@clerk/clerk-react";
 
 const Header = () => {
+  const { user, isSignedIn } = useUser();
   return (
     <div className="p-3 px-5 flex justify-between shadow-md">
       <img src="/logo.svg" alt="Logo" width={100} height={100} />
-      <Button>Get Started</Button>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <Link to="/auth/sign-in">
+          <Button>Get Started</Button>
+        </Link>
+      )}
     </div>
   );
 };
